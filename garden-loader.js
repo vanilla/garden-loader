e4f49f2053c5b581737b576060ee61fa7d6ef02d
Todd Burry <todd@vanillaforums.com>
2016-07-02 16:46:39 -0400

    Add debugging

--- garden-loader.js
+++ garden-loader.js
@@ -80,7 +80,7 @@
 
     function register(name, deps, wrapper) {
         if (Array.isArray(name)) {
-            console.log('System.register(anonymous)');
+            console.log('System.register(anonymous, %o)', deps);
             // anounymous module.
             anonymousEntry = [register];
             anonymousEntry.push.apply(anonymousEntry, arguments)
